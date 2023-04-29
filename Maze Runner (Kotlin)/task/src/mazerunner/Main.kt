9f29a9d7e9b7728e77a303d6629abc39f75c4e8a
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-04-29 19:26:17 -0400

    Stage 1

--- Maze Runner (Kotlin)/task/src/mazerunner/Main.kt
+++ Maze Runner (Kotlin)/task/src/mazerunner/Main.kt
@@ -1,58 +1,68 @@
 package mazerunner
 
-import kotlin.random.Random
-
-data class Cell(val row: Int, val col: Int)
-
 fun main() {
-    val maze = generateMaze(10, 10)
+    val maze = generateMaze(9, 9)
     printMaze(maze)
 }
 
-fun generateMaze(rows: Int, columns: Int): Array<IntArray> {
-    val maze = Array(rows) { IntArray(columns) { 1 } }
-    val entranceRow = Random.nextInt(1, rows - 1)
-    val exitRow = Random.nextInt(1, rows - 1)
-    maze[entranceRow][0] = 0
-    maze[exitRow][columns - 1] = 0
+fun generateBorderWalls(maze: Array<IntArray>): Array<IntArray> {
+    val width = maze.size
+    val height = maze[0].size
+    for (i in 0 until width) {
+        maze[i][0] = 1
+        maze[i][height - 1] = 1
+    }
+    for (j in 0 until height) {
+        maze[0][j] = 1
+        maze[width - 1][j] = 1
+    }
+    return maze
+}
 
-    val visited = Array(rows) { BooleanArray(columns) { false } }
-    val stack = mutableListOf<Cell>()
-    stack.add(Cell(entranceRow, 1))
+fun generateEnterAndExit(maze: Array<IntArray>): Array<IntArray> {
+    val width = maze.size
+    val height = maze[0].size
+    val enter = Pair(1, 0)
+    val exit = Pair(width - 2, height - 1)
+    maze[enter.first][enter.second] = 0
+    maze[exit.first][exit.second] = 0
+    return maze
+}
+
+
+fun generateMaze(width: Int, height: Int): Array<IntArray> {
+    val maze = Array(width) { i -> IntArray(height) { j -> if (i % 2 == 0 || j % 2 == 0) 1 else 0 } }
+    val start = Pair(1, 1)
+    val stack = mutableListOf(start)
+    val visited = mutableSetOf(start)
+    val directions = listOf(Pair(2, 0), Pair(0, 2), Pair(-2, 0), Pair(0, -2))
 
     while (stack.isNotEmpty()) {
-        val currentCell = stack.last()
-        val row = currentCell.row
-        val col = currentCell.col
-        visited[row][col] = true
-        maze[row][col] = 0
+        val current = stack.removeLast()
+        val unvisitedNeighbors = directions.map { Pair(current.first + it.first, current.second + it.second) }
+            .filter { it.first in 1 until width - 1 && it.second in 1 until height - 1 && it !in visited }
 
-        val neighbors = listOf(
-            Cell(row - 2, col),
-            Cell(row + 2, col),
-            Cell(row, col - 2),
-            Cell(row, col + 2)
-        ).shuffled()
-
-        var hasUnvisitedNeighbors = false
-        for (neighbor in neighbors) {
-            if (neighbor.row in 1 until (rows - 1) && neighbor.col in 1 until (columns - 1) && !visited[neighbor.row][neighbor.col]) {
-                val betweenRow = (row + neighbor.row) / 2
-                val betweenCol = (col + neighbor.col) / 2
-
-                maze[betweenRow][betweenCol] = 0
-                stack.add(neighbor)
-                hasUnvisitedNeighbors = true
-                break
-            }
-        }
-
-        if (!hasUnvisitedNeighbors) {
-            stack.removeLast()
+        if (unvisitedNeighbors.isNotEmpty()) {
+            val next = unvisitedNeighbors.random()
+            removeWalls(current, next, maze)
+            visited.add(next)
+            stack.add(current)
+            stack.add(next)
         }
     }
 
+    generateBorderWalls(maze)
+    generateEnterAndExit(maze)
+
     return maze
+}
+
+fun removeWalls(current: Pair<Int, Int>, next: Pair<Int, Int>, maze: Array<IntArray>) {
+    val xDiff = next.first - current.first
+    val yDiff = next.second - current.second
+    val removeX = current.first + xDiff / 2
+    val removeY = current.second + yDiff / 2
+    maze[removeX][removeY] = 0
 }
 
 fun printMaze(maze: Array<IntArray>) {
